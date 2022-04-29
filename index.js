e58690c96e6b5e5534ec09976fee7b208a29f233
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-04-29 16:04:59 +0600

    Server to mongoDb dataBase connect test success

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 require ('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Copy the code from Mongo Connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yzwpe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("fruitsHouse").collection("product");
+//   // perform actions on the collection object
+//   console.log("Thanks for Mongo DB connection bro");
+//   client.close();
+// });
+
+async function run(){
+    try {
+        await client.connect();
+        const fruitsCollection = client.db("fruitsHouse").collection("product");
+        app.get('/product', async(req, res)=>{
+            const query = {};
+            const cursor = fruitsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+    }
+    finally{}
+}
+run().catch(console.dir)
 
 // main root API
 app.get('/', (req, res)=>{
@@ -17,3 +45,6 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log('Listening to port', port);
 })
+
+// fruitsStock
+// Ajxpj8zrG9FSzJDX
